65fb132d586b23f7a15bece24ba2785572ea4e14
Tanusri Ghosh <tghosh25@gmail.com>
2023-02-03 21:55:51 +0530

    fix rubocop

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :post, foreign_key: 'post_id', class_name: 'Post'
 
   after_save :update_comments_counter
-
   def update_comments_counter
     post.increment!(:CommentsCounter)
   end
