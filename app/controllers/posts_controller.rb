4a49088443b548363fd08d4625d1fc3d4defa981
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2023-02-08 17:44:04 +0530

    Update following reviewer

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,47 @@
+# class PostsController < ApplicationController
+#   def index
+#     @user = User.find(params[:user_id])
+#     @posts = Post.where("author_id = #{params[:user_id]}")
+#     # @posts = @user.posts.includes(:comments)
+#   end
+
+#   def show
+#     @user = User.find(params[:user_id])
+#     @post = Post.includes([:author]).find(params[:id])
+#   end
+
+#   def new
+#     @post = Post.new
+#   end
+
+#   def create
+#     @post = current_user.posts.new(post_params)
+#     respond_to do |format|
+#       format.html do
+#         if @post.save
+#           redirect_to "/users/#{@post.author.id}/posts/#{@post.id}"
+#         else
+#           render :new
+#         end
+#       end
+#     end
+#   end
+
+#   private
+
+#   def post_params
+#     params.require(:post).permit(:title, :text, :likes_counter, :comments_counter)
+#   end
+# end
+
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    # @posts = Post.where("author_id = #{params[:user_id]}")
     @posts = @user.posts.includes(:comments)
   end
 
   def show
-    @user = User.find(params[:user_id])
-    @post = Post.includes([:author]).find(params[:id])
+    @post = Post.includes(comments: [:author]).find(params[:id])
   end
 
   def new
