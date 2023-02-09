0420f838ed61c5ad3677355d4e308bd145637d87
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2023-02-10 02:21:39 +0530

    Add files and folders for devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
+
   has_many :posts, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
-
   validates :name, presence: true
   validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-
   def last_three_posts
     posts.order(created_at: :desc).last(3)
   end
